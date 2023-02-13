cabd7feeb3fda68f3deedea557142e108abe4ec5
wanjirugachunji <wanjirugachunji@gmail.com>
2023-02-14 00:36:37 +0300

    commit tree 5253b31cac7462b52fefcc7c0da347e498cffa30

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
 class Student 
-
-end
+    require_relative './user'
+    
+    class Student < User
+        def initialize(knowledge = [])
+            @knowledge = knowledge
+        end
+    
+        def learn(piece)
+            @knowledge << piece
+        end
+    
+        def knowledge
+            @knowledge
+        end
+    
+    end
